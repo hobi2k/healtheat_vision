e1056f95f39c13094c4acb2a11f1240bda3e6031
root <root@ahnhs2k>
2025-12-23 01:44:37 +0900

    fix: 리팩토링 과정에서 생긴 syntax 오류 수정

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,7 @@
+"""
+실행 코드 오류로 ensure_dirs 삭제
+"""
+
 from .device import get_device
 from .logger import logger, init_logger
 from .paths import (
@@ -7,11 +11,9 @@ from .paths import (
     CONFIGS_DIR, 
     RUNS_DIR,
     MODELS_DIR,
-    ensure_dirs,
     ARTIFACTS_DIR,
     RAW_IMAGES_DIR,
     COLLECTED_IMAGES_DIR,
     CLASS_MAP_PATH
-    ensure_dirs
 )
 from .viz import set_korean_font
